bcd68cc8d147efae6dbc4999f7673fe15a2516a3
Fachri Hammad Fadhlullah Permadi <31992756+FachriHammadFP@users.noreply.github.com>
2018-05-07 19:16:04 +0700

    Update Model
    Penambahan variabel picture untuk model

--- models/card.py
+++ models/card.py
@@ -2,6 +2,8 @@ from odoo import api, fields, models
 from odoo.exceptions import ValidationError
 from datetime import datetime
 from datetime import timedelta
+from odoo import tools, _
+from odoo.modules.module import get_module_resource
 import logging
 
 class BusinessCard(models.Model):
@@ -18,13 +20,27 @@ class BusinessCard(models.Model):
 		_logger.debug('RESOURCE ID: ' + str(employee.name_related))
 		return employee.department_id
 
+
+
+
+	@api.model
+	def _default_image(self):
+		image_path = get_module_resource('hr','static/src/img','default_image.png')
+		return tools.image_resize_image_big(open(image_path,'rb').read().encode('base64')) 
+
 	#Employee Information
-	company_id = fields.Many2one('res.company', 'Company')
+	company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.user,readonly=True)
 	employee_ids = fields.Many2one('res.users', string='Employee', track_visibility='onchange', default=lambda self: self.env.user,readonly=True)
 	department_id = fields.Many2one('hr.department', string='Department', default=_current_department, readonly=True)
-	job_title = fields.Char(string='Job Title')
+	job_title = fields.Many2one('hr.job', string='Job Title', default=lambda self: self.env.user, readonly=True)
 	
-	#Card Information
+
+	image = fields.Binary("Photo", default=_default_image, attachment=True, help="This field holds the image used as photo for the employee, limited to 1024x1024px.")
+	image_medium = fields.Binary("Medium-sized photo", attachment=True,
+        help="Medium-sized photo of the employee. It is automatically "
+             "resized as a 128x128px image, with aspect ratio preserved. "
+             "Use this field in form views or some kanban views.")
+	#Card Informationr
 	card_Type = fields.Selection((('business_card','Business Card'),('id_card','Id Card')),string='Card Type')
 	card_Id = fields.Integer(string='Card No')
 	request_Date= fields.Date(string='Request Date', default=datetime.now())
@@ -39,4 +55,12 @@ class BusinessCard(models.Model):
         ('closed', 'Closed'),
         ('reject', 'Reject'),
         ], string='Status', readonly=True, copy=False, index=True, track_visibility='onchange', default='new')
+
+
+
+
+
+
+    	
+    
 
