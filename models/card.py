39354d1a7462ee2c79bfbecea8043d019845a7de
Fakhri W N <fakhri.waliyyuddin.tif16@polban.ac.id>
2018-05-07 21:28:11 +0700

    Perbaikan View dan get data

--- models/card.py
+++ models/card.py
@@ -20,8 +20,25 @@ class BusinessCard(models.Model):
 		_logger.debug('RESOURCE ID: ' + str(employee.name_related))
 		return employee.department_id
 
+	@api.model
+	def _current_user(self):
 
+		resource = self.env['resource.resource'].search([('user_id','=', self.env.uid)])[0]
+		employee = self.env['hr.employee'].search([('resource_id','=', resource.id)])[0]
 
+		_logger = logging.getLogger(__name__)
+		_logger.debug('RESOURCE ID: ' + str(employee.name_related))
+		return employee.id
+
+	@api.model
+	def _current_job(self):
+
+		resource = self.env['resource.resource'].search([('user_id','=', self.env.uid)])[0]
+		employee = self.env['hr.employee'].search([('resource_id','=', resource.id)])[0]
+
+		_logger = logging.getLogger(__name__)
+		_logger.debug('RESOURCE ID: ' + str(employee.name_related))
+		return employee.job_id
 
 	@api.model
 	def _default_image(self):
@@ -29,11 +46,11 @@ class BusinessCard(models.Model):
 		return tools.image_resize_image_big(open(image_path,'rb').read().encode('base64')) 
 
 	#Employee Information
-	company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.user,readonly=True)
-	employee_ids = fields.Many2one('res.users', string='Employee', track_visibility='onchange', default=lambda self: self.env.user,readonly=True)
+	company_id = fields.Many2one('res.company', 'Company', default=_current_user,readonly=True)
+	employee_ids = fields.Many2one('res.users', string='Employee', track_visibility='onchange', default=_current_user,readonly=True)
 	department_id = fields.Many2one('hr.department', string='Department', default=_current_department, readonly=True)
-	job_title = fields.Many2one('hr.job', string='Job Title', default=lambda self: self.env.user, readonly=True)
-	
+	job_title = fields.Many2one('hr.job', string='Job Title', default=_current_job, readonly=True)
+	address_home_id = fields.Many2one('res.partner', string='Home Address')
 
 	image = fields.Binary(string="Photo", default=_default_image, attachment=True, help="This field holds the image used as photo for the employee, limited to 1024x1024px.")
 	image_medium = fields.Binary("Medium-sized photo", attachment=True,
@@ -41,9 +58,9 @@ class BusinessCard(models.Model):
              "resized as a 128x128px image, with aspect ratio preserved. "
              "Use this field in form views or some kanban views.")
 	#Card Informationr
-	card_Type = fields.Selection((('business_card','Business Card'),('id_card','Id Card')),string='Card Type')
+	card_Type = fields.Selection((('business_card','Business Card'),('id_card','Id Card')),string='Card Type', required=True)
 	card_Id = fields.Integer(string='Card No')
-	request_Date= fields.Date(string='Request Date', default=datetime.now())
+	request_Date= fields.Date(string='Request Date', default=datetime.now(), readonly=True)
 	description = fields.Text('Notes',required=True)
 	status	= fields.Selection((('permanent','Permanent'),('temporary','Temporary')),string='Status')
 	active_Periode = fields.Date(string='Active Periode')
