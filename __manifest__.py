3fe5b54c24b58e3d5b3050bd66cfbab8b2862b70
Nino <ninomaryono38@gmail.com>
2018-05-14 17:16:51 +0700

    add security access rules for employee and officer

--- __manifest__.py
+++ __manifest__.py
@@ -2,15 +2,16 @@
 	'name': 'Employee Card',
 	'description': 'Crete Your Own Employee Card',
 	'author': 'KELOMPOK B2',
-	'depends': ['base', 'mail', 'hr', 'report'],
+	'depends': ['base', 'mail', 'hr', 'report', 'barcodes'],
 	'data': [
+		'security/card_security.xml',
+		'security/ir.model.access.csv',
 		'reports/print_card.xml',
 		'reports/report_card_template.xml',
 		'reports/print_business_card.xml',
 		'reports/report_business_card_template.xml',
 		'views/card_menu.xml',
-		'views/card_view.xml',
-		
+		'views/card_view.xml'
 	],
 	'application': False,
 }
