ae842e13566a6eb4f817b8c880cb06f3bbf79d83
PEGA-K /cyber pegasus <pega.kurniawan.02@gmail.com>
2018-05-22 11:58:01 +0700

    disable

--- models/card.py
+++ models/card.py
@@ -10,7 +10,7 @@ class PrintCard(models.Model):
 	_name = 'hr.card'
 	_description = 'Model for print card.'
 	_inherit = ['mail.thread']
-	
+
 	@api.model
 	def _current_department(self):
 		resource = self.env['resource.resource'].search([('user_id','=', self.env.uid)])[0]
@@ -34,14 +34,14 @@ class PrintCard(models.Model):
 		_logger = logging.getLogger(__name__)
 		_logger.debug('RESOURCE ID: ' + str(employee.name_related))
 		return employee.job_id
-		
+
 
 	#Employee Information
-	
+
 	employee_ids = fields.Many2one('hr.employee', string='Employee', track_visibility='onchange', default=_current_user, readonly=True)
 	department_id = fields.Many2one('hr.department', string='Department', default=_current_department, readonly=True)
 	job_title = fields.Many2one('hr.job', string='Job Title', default=_current_job, readonly=True)
-	
+
 	logo = fields.Binary(string="Company Logo", default=_current_user, readonly=True)
 
 
@@ -49,7 +49,7 @@ class PrintCard(models.Model):
 	card_Type = fields.Selection((
 		('business_card', 'Business Card'),
 		('id_card','Id Card')
-	), string='Card Type', required=True)
+	), string='Card Type', required=True, readonly=True)
 	card_Id = fields.Char(string='Card No')
 	request_Date= fields.Date(string='Requested Date', default=datetime.now(), readonly=True)
 	description = fields.Text('Notes', required=True)
